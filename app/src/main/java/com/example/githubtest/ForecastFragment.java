66febf2367a8634e87d3ad15ac9c8cf8a39c9627
CaiBindi <1091110955@qq.com>
2020-06-09 18:12:03 +0800

    实现简单的预测功能

--- app/src/main/java/com/example/githubtest/ForecastFragment.java
+++ app/src/main/java/com/example/githubtest/ForecastFragment.java
@@ -1,15 +1,35 @@
 package com.example.githubtest;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.example.githubtest.SQL.BTConnection;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 /**
@@ -29,6 +49,7 @@ public class ForecastFragment extends Fragment {
 
     private TextView tv_result;
     private Button btn_forecast;
+    private String s;
 
     public ForecastFragment() {
         // Required empty public constructor
@@ -80,6 +101,54 @@ public class ForecastFragment extends Fragment {
     }
 
     private void userForecast(){
+        SharedPreferences preferences = getActivity().getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
+        int userid = preferences.getInt("userid", 0);
+        String my_mac = getLocalMacAddress();
 
+        OkHttpClient client = new OkHttpClient();
+        Calendar no = Calendar.getInstance();
+        no.set(Calendar.DATE, no.get(Calendar.DATE) - 28);
+        String time = BTConnection.DateToString(no.getTime());
+        Log.d("getForecastTest", "time: "+time);
+        FormBody body = new FormBody.Builder()
+                .add("userid", String.valueOf(userid))
+                .add("selfmac","iiiii")   //getLocalMacAddress()
+                .add("date",time)
+                .build();
+        Request request = new Request.Builder()
+                .url("http://39.97.163.234:8443/api/bluetoothInfo/getForecast")
+                .post(body)
+                .build();
+
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+                Log.d("getForecastTest", "onFailure: 访问服务器失败");
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                s = response.body().string();
+                Log.d("getForecastTest", "onResponse: " + s);
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        double result = Double.parseDouble(s);
+                        double r = new BigDecimal(result*100).setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
+                        tv_result.setText("经预测分析，您的感染概率为:"+String.valueOf(r)+"%");
+                    }
+                });
+            }
+        });
+
+    }
+
+    public String getLocalMacAddress() {
+        WifiManager wifi = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = wifi.getConnectionInfo();
+        return info.getMacAddress();
     }
 }
