0c442a2ae6163dedff81208a44d22d002fc1ea92
vgril <1152235194@qq.com>
2020-06-10 15:51:51 +0800

    更改蓝牙扫描为前台服务

--- app/src/main/java/com/example/githubtest/HomeFragment.java
+++ app/src/main/java/com/example/githubtest/HomeFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -144,7 +145,12 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 if(!btn_bluetooth.isSelected()){
-                    getActivity().startService(serviceIntent);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        //android8.0以上通过startForegroundService启动service
+                        getActivity().startForegroundService(serviceIntent);
+                    } else {
+                        getActivity().startService(serviceIntent);
+                    }
                     onBlueTooth();
                 }else{
                     getActivity().stopService(serviceIntent);
